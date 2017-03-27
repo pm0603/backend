7f24d98d40476d7ef4c8f0cf08101ff05764aa51
chumjikim <kcg9077@gmail.com>
2017-03-27 22:17:24 +0900

    django-rest-auth설치, rest_auth이용한 member의 model생성, migrate

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/1.10/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
-import os
 import json
+import os
 
 DEBUG = True
 
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'member.apps.MemberConfig',
+    'rest_framework',
+    'rest_auth',
 ]
 
 MIDDLEWARE = [
@@ -92,7 +95,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'member.MyUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        user = MyUser(email=email)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save()
+
+        return user
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    joined_date = models.DateTimeField(auto_now_add=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
