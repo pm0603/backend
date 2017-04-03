178743f8ed10ca039e28835fd1672754d2d07f80
YM Choi <archoiym@gmail.com>
2017-04-03 09:33:43 +0900

    urls.py에 static 관련 설정 추가.

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -26,4 +28,4 @@ urlpatterns = [
     # open api 확인용 url
     url(r'^api/', include('openapi.urls')),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
