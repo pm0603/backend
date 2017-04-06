e064e37d39feeda65db86d6c19a96ba8307821d3
YM Choi <archoiym@gmail.com>
2017-04-06 13:53:11 +0900

    PerformanceSerializer 로 수정.

--- django_app/openapi/serializers.py
+++ django_app/openapi/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 
-from openapi.models import Content
+from openapi.models import Performance
 
 
-class ContentSerializer(serializers.ModelSerializer):
+class PerformanceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Content
+        model = Performance
         fields = ('seq', 'title', 'start_date', 'end_date', 'place', 'realm_name',
                   'area', 'price', 'content', 'ticket_url', 'phone', 'thumbnail',
                   'place_url', 'place_addr', 'gps_x', 'gps_y')
--- django_app/openapi/views.py
+++ django_app/openapi/views.py
@@ -2,33 +2,33 @@ from rest_framework import filters
 from rest_framework import viewsets
 
 from openapi.models import Performance
-from .serializers import ContentSerializer
+from .serializers import PerformanceSerializer
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
     queryset = Performance.objects.all()
-    serializer_class = ContentSerializer
+    serializer_class = PerformanceSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('seq', 'title', 'start_date', 'end_date', 'place', 'realm_name',
                      'area', 'price', 'phone')
 
 class DetailViewSet(viewsets.ModelViewSet):
     queryset = Performance.objects.all()
-    serializer_class = ContentSerializer
+    serializer_class = PerformanceSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('seq', 'title')
 
 
 class AreaViewSet(viewsets.ModelViewSet):
     queryset = Performance.objects.all()
-    serializer_class = ContentSerializer
+    serializer_class = PerformanceSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('area', 'area')
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Performance.objects.all()
-    serializer_class = ContentSerializer
+    serializer_class = PerformanceSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('realm_name', 'realm_name')
 
