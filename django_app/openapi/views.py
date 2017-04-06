9a7e5297dcc637b99f3928b53720dd9da7214257
YM Choi <archoiym@gmail.com>
2017-04-06 13:50:06 +0900

    Performance로 앱이름 바꾸는 바람에 여기저기 바꿀게 많네요.

--- django_app/openapi/views.py
+++ django_app/openapi/views.py
@@ -1,33 +1,33 @@
 from rest_framework import filters
 from rest_framework import viewsets
 
-from openapi.models import Content
+from openapi.models import Performance
 from .serializers import ContentSerializer
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
-    queryset = Content.objects.all()
+    queryset = Performance.objects.all()
     serializer_class = ContentSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('seq', 'title', 'start_date', 'end_date', 'place', 'realm_name',
                      'area', 'price', 'phone')
 
 class DetailViewSet(viewsets.ModelViewSet):
-    queryset = Content.objects.all()
+    queryset = Performance.objects.all()
     serializer_class = ContentSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('seq', 'title')
 
 
 class AreaViewSet(viewsets.ModelViewSet):
-    queryset = Content.objects.all()
+    queryset = Performance.objects.all()
     serializer_class = ContentSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('area', 'area')
 
 
 class GenreViewSet(viewsets.ModelViewSet):
-    queryset = Content.objects.all()
+    queryset = Performance.objects.all()
     serializer_class = ContentSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('realm_name', 'realm_name')
